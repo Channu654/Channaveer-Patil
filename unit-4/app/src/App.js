0b06fa4de4e27293e2ac7df362a5593073365d39
channaveer <channaveerpatil999@gmail.com>
2022-04-10 18:56:54 +0530

    Problem1/day1

--- unit-4/app/src/App.js
+++ unit-4/app/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import {Counter} from './sprint-1/day-2/assignments/Counter/Counter'
+import First from './sprint-1/day-1/assignments/Problem1/First';
+// import {Counter} from './sprint-1/day-2/assignments/Counter/Counter'
 
 function App() {
   return (
     <div>
-      <Counter/>
-      
+      {/* <Counter/> */}
+      <First/>
     </div>
   );
 }
---
+++ unit-4/app/src/sprint-1/day-1/assignments/Problem1/First.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function First (){
+    return(
+        <>
+        <div>
+        <h1>Mobile Operating System</h1>
+        <ul>
+            <li>Android</li>
+            <li>Blackberry</li>
+            <li>iPhone</li>
+            <li>Window Phone</li>
+
+        </ul>
+        </div>
+        <div>
+        <h1>Mobile MAnufactures</h1>
+        <ul>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+
+        </ul>
+        </div>
+        </>
+    )
+}
+export default First
