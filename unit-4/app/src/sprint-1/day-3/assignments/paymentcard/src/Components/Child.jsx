a88aa2266f1c8969cb8f1442996832a942331713
channaveer <channaveerpatil999@gmail.com>
2022-04-17 16:54:02 +0530

    React_PaymentCrad

---
+++ unit-4/app/src/sprint-1/day-3/assignments/paymentcard/src/Components/Child.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from './child.module.css'
+const Child = () => {
+  return <div>
+      <div className="box"> 
+        
+      </div>
+  </div>;
+};
+
+export default Child;
