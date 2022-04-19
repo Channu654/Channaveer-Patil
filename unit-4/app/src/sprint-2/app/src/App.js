32144a71ef1d14a91556b58244c2687f9f306c00
channaveer <channaveerpatil999@gmail.com>
2022-04-19 09:21:39 +0530

    React_FormApp

--- unit-4/app/src/sprint-2/app/src/App.js
+++ unit-4/app/src/sprint-2/app/src/App.js
@@ -1,13 +1,16 @@
 
 import './App.css';
-import { AddHouse } from './C2_HouseRent_App/AddHouse';
-import { Rentals } from './C2_HouseRent_App/Rentals';
-
+import Form from './day-4/Form';
+// import { AddHouse } from './C2_HouseRent_App/AddHouse';
+// import { Rentals } from './C2_HouseRent_App/Rentals';
+// import {Todo} from './day2/TodoApp/Components/Todo'
 function App() {
   return (
     <div className="App">
-     <AddHouse/>
-     <Rentals/>
+      {/* <Todo/> */}
+     {/* <AddHouse/>
+     <Rentals/> */}
+     <Form/>
     </div>
   );
 }
---
+++ unit-4/app/src/sprint-2/app/src/day-4/Form.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+
+const Form = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleChange1 = (e) => {
+    setPassword(e.target.value);
+  };
+  // for storing in array user entry her
+  const [allentry, setAllEntry] = useState([]);
+
+  const submitFomm = (e) => {
+    e.preventDefault();
+    const newentry = { email: email, password: password };
+
+    setAllEntry([...allentry, newentry]);
+    console.log(allentry);
+  };
+  return (
+    <>
+      <form action='' onSubmit={submitFomm}>
+        <div>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='text'
+            name='email'
+            value={email}
+            id='email'
+            autoComplete='off'
+            required
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='text'
+            name='password'
+            value={password}
+            id='password'
+            autoComplete='off'
+            required
+            onChange={handleChange1}
+          />
+        </div>
+        <button type='submit'>submit</button>
+      </form>
+      <div>  
+         {
+             allentry .map((el)=>{
+                 return (
+                     <div className='ShowDtat'>
+                     <p>{el.email}</p> 
+                    <p>{el.password}</p>  
+                     </div>
+                    
+
+                 )
+             })
+         }
+      </div>
+    </>
+  );
+};
+
+export default Form;
