8e7cf245297367b3df309510dd3169cd4dd3a234
channaveer <channaveerpatil999@gmail.com>
2022-04-22 09:50:04 +0530

    Google Timer

--- unit-4/app/src/sprint-2/app/src/App.js
+++ unit-4/app/src/sprint-2/app/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Form from './day-4/Form/Form';
 import Form1 from './day-4/Form1';
+import Timer from './day-4/Google-Timer/Components/Timer';
 // import { AddHouse } from './C2_HouseRent_App/AddHouse';
 // import { Rentals } from './C2_HouseRent_App/Rentals';
 // import {Todo} from './day2/TodoApp/Components/Todo'
@@ -12,7 +13,8 @@ function App() {
      {/* <AddHouse/>
      <Rentals/> */}
      {/* <Form1/> */}
-     <Form/>
+     {/* <Form/> */}
+     <Timer/>
     </div>
   );
 }
