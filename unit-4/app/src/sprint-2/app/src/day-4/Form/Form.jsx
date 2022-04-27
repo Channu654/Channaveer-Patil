ab661eaf8f405164a00358e88fcf003789829f0d
channaveer <channaveerpatil999@gmail.com>
2022-04-27 22:49:22 +0530

    timer

--- unit-4/app/src/sprint-2/app/src/day-4/Form/Form.jsx
+++ unit-4/app/src/sprint-2/app/src/day-4/Form/Form.jsx
@@ -1,132 +1,55 @@
-import React, { useState } from 'react';
-import styles from './form.module.css';
-
-const Form = () => {
-  const [formData, setFormData] = useState({
-    Name:"",
-    Age:"",
-    Address:"",
-    Department:"",
-    material:"",
-    showpassword:false,
-  });
-
-  const handleChange = (e) => {
-    const inputName = e.target.name;
-    // console.log('inputName:', inputName)
-
-    //checkbox
-    if (e.target.type === 'checkbox') {
-      //  console.log('e.target.type:', e.target.type  ,e.target.checked)
-
-      setFormData({
-        ...formData,
-        [inputName]: e.target.checked,
-      });
-    }
-    else if(e.target.type==="file"){
-      setFormData({
-        ...formData,
-        [inputName]:e.target.files,
-      })
-    }
-    else{
-      setFormData({
-        ...formData,
-        [inputName]: e.target.value,
-      });
-    }
-    
-  };
-  const formSubmit = (e) => {};
-
-  return (
-    <div className={styles.form}>
-      <form onSubmit={formSubmit}>
-        <div className={styles.grid}>
-          <label htmlFor=''> Aame</label>
-          <input type='text' name='name' onChange={handleChange} />
-        </div>
-        <div className={styles.grid}>
-          <label htmlFor=''> password</label>
-          <input type={formData.showpassword ? "text":"password"} name='password' onChange={handleChange} />
-        </div>
-        <div className={styles.grid}>
-          <label htmlFor=''> showpassword</label>
-          <input type='checkbox' name='showpassword' onChange={handleChange} />
-        </div>
-        <div className={styles.grid}>
-          <label htmlFor=''> Age</label>
-          <input type='number' name='age' onChange={handleChange} />
-        </div>
-        <div className={styles.grid}>
-          <label htmlFor=''> Dob</label>
-          <input type='date' name='dob' onChange={handleChange} />
-        </div>
-        <div>
-          <input type='submit' name='submit' onChange={handleChange} />
-        </div>
-      </form>
-    </div>
-  );
-};
-
-export default Form;
-
-
-
-
 // import React, { useState } from 'react';
 // import styles from './form.module.css';
 
 // const Form = () => {
 //   const [formData, setFormData] = useState({
-//     username:"",
-//     password:"",
+//     Name:"",
+//     Age:"",
+//     Address:"",
+//     Department:"",
+//     material:"",
 //     showpassword:false,
 //   });
 
 //   const handleChange = (e) => {
 //     const inputName = e.target.name;
-//     // checkbox
+//     // console.log('inputName:', inputName)
+
+//     //checkbox
 //     if (e.target.type === 'checkbox') {
-//       console.log(e.target.value, e.target.checked);
+//       //  console.log('e.target.type:', e.target.type  ,e.target.checked)
 
 //       setFormData({
 //         ...formData,
 //         [inputName]: e.target.checked,
 //       });
 //     }
-//     //files
-//     else if (e.target.type === 'file') {
-//       console.log(e.target.value, e.target.files);
+//     else if(e.target.type==="file"){
 //       setFormData({
 //         ...formData,
-//         [inputName]: e.target.files,
-//       });
+//         [inputName]:e.target.files,
+//       })
 //     }
-//      else {
+//     else{
 //       setFormData({
 //         ...formData,
 //         [inputName]: e.target.value,
 //       });
 //     }
+    
 //   };
+//   const formSubmit = (e) => {};
 
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//   };
 //   return (
 //     <div className={styles.form}>
-//       Form
-//       <form onSubmit={handleSubmit}>
+//       <form onSubmit={formSubmit}>
 //         <div className={styles.grid}>
-//           <label htmlFor=''> username</label>
-//           <input type='text' name='username' onChange={handleChange} />
+//           <label htmlFor=''> Aame</label>
+//           <input type='text' name='name' onChange={handleChange} />
 //         </div>
 //         <div className={styles.grid}>
 //           <label htmlFor=''> password</label>
-//           <input type={formData.showpassword ? "text" : "password"} name='password' onChange={handleChange} />
+//           <input type={formData.showpassword ? "text":"password"} name='password' onChange={handleChange} />
 //         </div>
 //         <div className={styles.grid}>
 //           <label htmlFor=''> showpassword</label>
@@ -134,18 +57,14 @@ export default Form;
 //         </div>
 //         <div className={styles.grid}>
 //           <label htmlFor=''> Age</label>
-//           <input type='number' name='Age' onChange={handleChange} />
+//           <input type='number' name='age' onChange={handleChange} />
 //         </div>
 //         <div className={styles.grid}>
-//           <label htmlFor=''> DOB</label>
+//           <label htmlFor=''> Dob</label>
 //           <input type='date' name='dob' onChange={handleChange} />
 //         </div>
-//         <div className={styles.grid}>
-//           <label htmlFor=''> Resume</label>
-//           <input type='file' name='resume' onChange={handleChange} />
-//         </div>
-//         <div className={styles.grid}>
-//           <input type='submit' name='submit' />
+//         <div>
+//           <input type='submit' name='submit' onChange={handleChange} />
 //         </div>
 //       </form>
 //     </div>
@@ -153,3 +72,84 @@ export default Form;
 // };
 
 // export default Form;
+
+
+
+
+import React, { useState } from 'react';
+import styles from './form.module.css';
+
+const Form = () => {
+  const [formData, setFormData] = useState({
+    username:"",
+    password:"",
+    showpassword:false,
+  });
+
+  const handleChange = (e) => {
+    const inputName = e.target.name;
+    // checkbox
+    if (e.target.type === 'checkbox') {
+      console.log(e.target.value, e.target.checked);
+
+      setFormData({
+        ...formData,
+        [inputName]: e.target.checked,
+      });
+    }
+    //files
+    else if (e.target.type === 'file') {
+      console.log(e.target.value, e.target.files);
+      setFormData({
+        ...formData,
+        [inputName]: e.target.files,
+      });
+    }
+     else {
+      setFormData({
+        ...formData,
+        [inputName]: e.target.value,
+      });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className={styles.form}>
+      Form
+      <form onSubmit={handleSubmit}>
+        <div className={styles.grid}>
+          <label htmlFor=''> username</label>
+          <input type='text' name='username' onChange={handleChange} />
+        </div>
+        <div className={styles.grid}>
+          <label htmlFor=''> password</label>
+          <input type={formData.showpassword ? "text" : "password"} name='password' onChange={handleChange} />
+        </div>
+        <div className={styles.grid}>
+          <label htmlFor=''> showpassword</label>
+          <input type='checkbox' name='showpassword' onChange={handleChange} />
+        </div>
+        <div className={styles.grid}>
+          <label htmlFor=''> Age</label>
+          <input type='number' name='Age' onChange={handleChange} />
+        </div>
+        <div className={styles.grid}>
+          <label htmlFor=''> DOB</label>
+          <input type='date' name='dob' onChange={handleChange} />
+        </div>
+        <div className={styles.grid}>
+          <label htmlFor=''> Resume</label>
+          <input type='file' name='resume' onChange={handleChange} />
+        </div>
+        <div className={styles.grid}>
+          <input type='submit' name='submit' />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
