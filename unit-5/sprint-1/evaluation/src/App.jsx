1154ecdb42a96eab0c5c0e06e3c8c520c994ab85
channaveer <channaveerpatil999@gmail.com>
2022-05-23 19:23:12 +0530

    initial
    initial

---
+++ unit-5/sprint-1/evaluation/src/App.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import Button from './components/Button';
+import CandidateCard from './components/CandidateCard';
+import axios from 'axios';
+import './styles.css';
+
+export default function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, seterror] = useState(false);
+  const [page, setPage] = useState(1);
+  const [sort, setsort] = useState('ASC');
+
+  useEffect(() => {
+    setLoading(true);
+    axios(
+      {
+        method: 'GET',
+        url: 'https://json-server-mocker-masai.herokuapp.com/candidates',
+        params: {
+          _page: page,
+          _limit: 5,
+          _sort: 'salary',
+          _order: sort,
+        },
+      },
+      [page, sort]
+    )
+      .then((res) => {
+        setData(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        seterror(true);
+        setLoading(false);
+      });
+  }, [page, sort]);
+
+  console.log('data:', data);
+
+  return (
+    <div className='App'>
+      <div>
+        {loading && <div id='loading-container'>...Loading</div>}
+        {sort === 'DESC' ? (
+          <Button
+            id='SORT_BUTTON'
+            onClick={() => setsort('ASC')}
+            title={`Sort by Ascending Salary`}
+          />
+        ) : (
+          <Button
+            id='SORT_BUTTON'
+            onClick={() => setsort('DESC')}
+            title={`Sort by Dscending Salary`}
+          />
+        )}
+
+        <Button
+          disabled={page === 1}
+          onClick={() => setPage(page - 1)}
+          title='PREV'
+          id='PREV'
+        />
+        <Button id='NEXT' onClick={() => setPage(page + 1)} title='NEXT' />
+      </div>
+      {data.map((item) => (
+        <CandidateCard key={item.id} {...item} />
+      ))}
+    </div>
+  );
+}
---
+++ unit-5/sprint-1/evaluation/src/components/CandidateCard.jsx
@@ -0,0 +1,24 @@
+import styles from "./CandidateCard.module.css";
+
+function CandidateCard({
+  avatar: url,
+  name,
+  company_name:title,
+  salary
+}) {
+
+
+
+  return (
+    <div data-testid="candidate-container" className={styles.container}>
+      <img alt="logo" width="100px" height="100px" src={url} />
+      <div>
+        <div>Name : {name}</div>
+        <div>Title & Company Name : {title}</div>
+      </div>
+      <div>{salary} $ Salary</div>
+    </div>
+  );
+}
+
+export default CandidateCard;
