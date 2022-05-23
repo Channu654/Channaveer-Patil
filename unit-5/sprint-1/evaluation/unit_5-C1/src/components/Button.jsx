d8784dd69dbe7751183cdf1ed08bf6e10e3888fb
channaveer <channaveerpatil999@gmail.com>
2022-05-23 16:59:54 +0530

    unit-5 c1

---
+++ unit-5/sprint-1/evaluation/unit_5-C1/src/components/Button.jsx
@@ -0,0 +1,16 @@
+import styles from './Button.module.css';
+
+function Button({ title, onClick, disabled, id }) {
+  return (
+    <button
+      id={id}
+      onClick={onClick}
+      disabled={disabled}
+      data-testid='button-component'
+      className={styles.button}>
+      {title}
+    </button>
+  );
+}
+
+export default Button;
