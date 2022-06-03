ccbc158179f1c771a94c2a5b9961d15e702ed8d2
channaveer <channaveerpati999@gmail.com>
2022-06-03 07:45:19 +0530

    Counter App_reducer

--- unit-5/sprint-3/day-2/todo_app-redux/src/App.js
+++ unit-5/sprint-3/day-2/todo_app-redux/src/App.js
@@ -5,7 +5,7 @@ import { TodoList } from './component/TodoList';
 function App() {
   return (
     <div className='App'>
-      <h2>Redux</h2>
+      <h2>Redux12</h2>
       <Todoinput />
       <TodoList />
     </div>
