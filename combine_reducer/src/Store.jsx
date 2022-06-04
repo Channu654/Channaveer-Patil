f7ec8a891fe90a4a11fa13f4522d54cea2f443bd
channaveer <channaveerpati999@gmail.com>
2022-06-04 08:11:23 +0530

    redux

---
+++ combine_reducer/src/Store.jsx
@@ -0,0 +1,16 @@
+import { combineReducers, legacy_createStore as createStore } from 'redux';
+import { counterreducer } from './Redux/counter/Reducer';
+import { todosReducer } from './Redux/Todos/Reducer';
+//4. store  > app.jsx
+
+// const inistate = { count: 0 };
+
+const rootReducer = combineReducers({
+  counter: counterreducer,
+  todosReducer: todosReducer,
+});
+
+const store = createStore(rootReducer);
+console.log('store:', store.getState());
+
+export { store };
